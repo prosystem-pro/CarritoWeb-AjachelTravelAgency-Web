79466c7b97272d6506925bc4a45a00dbb0a55c12
prosystem-pro <prosystem490@gmail.com>
2026-03-24 01:15:34 -0600

    Nuevos Cambios

--- src/app/Entornos/Entorno.prod.ts
+++ src/app/Entornos/Entorno.prod.ts
@@ -1,5 +1,5 @@
 export const Entorno = {
   Produccion: true,
   ApiUrl: 'https://carritoweb-ajacheltravelagency-api-production.up.railway.app/api/',
-  NombreEmpresa: 'Vendedor'
+  NombreEmpresa: 'AjachelTravelAgency'
 };
